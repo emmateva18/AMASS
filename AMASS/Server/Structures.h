1fbdd7461a84c337a063fb33896e49abaa7a890e
kamilanov18 <54852657+kamilanov18@users.noreply.github.com>
2021-03-03 17:27:28 +0200

    Move save() function defenitions from Structures.h to Structures.cpp
    * Add and move function definitions to Structures.cpp

--- AMASS/Server/Structures.h
+++ AMASS/Server/Structures.h
@@ -2,7 +2,6 @@
 #include "Libraries.h"
 #include "BasicFileOperations.h"
 
-
 enum STATUS
 {
 	pendingApproval,
@@ -15,12 +14,7 @@ struct ROLE
 {
 	int id = -1;
 	std::string name = "teamless";
-
-	void save(std::fstream& file)
-	{
-		saveInt(file, id);
-		saveStr(file, name);
-	}
+	void save(std::fstream& file);
 };
 
 // Name and surname, class, role which the student prefers to carry out in the team, email.
@@ -33,16 +27,7 @@ struct STUDENT
 	std::string grade;
 	std::string email;
 	bool isInTeam = false;
-
-	void save(std::fstream& file)
-	{
-		saveStr(file, firstName);
-		saveStr(file, middleName);
-		saveStr(file, surname);
-		saveStr(file, grade);
-		saveStr(file, email);
-		saveBool(file, isInTeam);
-	}
+	void save(std::fstream& file);
 };
 
 struct TEACHER
@@ -53,15 +38,7 @@ struct TEACHER
 	std::string email;
 	// The teams' ids will be compared instead of wasting space for TEAM structs
 	std::vector<int> teamIds = {};
-
-	void save(std::fstream& file)
-	{
-		saveStr(file, firstName);
-		saveStr(file, middleName);
-		saveStr(file, surname);
-		saveStr(file, email);
-		saveVec(file, teamIds);
-	}
+	void save(std::fstream& file);
 };
 
 struct DATE
@@ -69,25 +46,14 @@ struct DATE
 	int day;
 	int month;
 	int year;
-
-	void save(std::fstream& file)
-	{
-		saveInt(file, day);
-		saveInt(file, month);
-		saveInt(file, year);
-	}
+	void save(std::fstream& file);
 };
 
 struct TEAM_MEMBER
 {
 	std::string studentEmail;
 	int roleId;
-
-	void save(std::fstream& file)
-	{
-		saveStr(file, studentEmail);
-		saveInt(file, roleId);
-	}
+	void save(std::fstream& file);
 };
 
 struct TEAM
@@ -98,23 +64,7 @@ struct TEAM
 	DATE dateOfSetUp;
 	STATUS status = STATUS::pendingApproval;
 	std::vector<TEAM_MEMBER> members;
-
-	void save(std::fstream& file)
-	{
-		saveInt(file, id);
-		saveStr(file, name);
-		saveStr(file, desc);
-		dateOfSetUp.save(file);
-		saveInt(file, status);
-
-		uint16_t size = (uint16_t)members.size();
-		file.write((const char*)&size, sizeof(size));
-
-		for (uint16_t i = 0; i < size; i++)
-		{
-			members[i].save(file);
-		}
-	}
+	void save(std::fstream& file);
 };
 
 struct SCHOOL
@@ -125,33 +75,5 @@ struct SCHOOL
 	std::vector<TEACHER> teachers;
 	std::vector<TEAM> teams;
 	std::vector<STUDENT> students;
-
-	void save(std::fstream& file)
-	{
-		saveStr(file, name);
-		saveStr(file, city);
-		saveStr(file, address);
-
-		uint16_t sizeTachers = (uint16_t)teachers.size();
-		uint16_t sizeTeams = (uint16_t)teams.size();
-		uint16_t sizeStudents = (uint16_t)students.size();
-
-		file.write((const char*)&sizeTachers, sizeof(sizeTachers));
-		for (uint16_t i = 0; i < sizeTachers; i++)
-		{
-			teachers[i].save(file);
-		}
-
-		file.write((const char*)&sizeTeams, sizeof(sizeTeams));
-		for (uint16_t i = 0; i < sizeTeams; i++)
-		{
-			teams[i].save(file);
-		}
-
-		file.write((const char*)&sizeStudents, sizeof(sizeStudents));
-		for (uint16_t i = 0; i < sizeStudents; i++)
-		{
-			students[i].save(file);
-		}
-	}
+	void save(std::fstream& file);
 };
